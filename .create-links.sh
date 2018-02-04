061dbf4459ccae4d8374ee884660d21a5d807c47
Kos <spiritofsands@protonmail.com>
2018-02-04 15:01:36 +0200

    contitional exclude of some links

--- .create-links.sh
+++ .create-links.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-ln -s ~/bin/firefox-stable/firefox ~/bin/firefox
+if [[ "$HOSTNAME" == 'kos-pc' ]]; then
+  ln -s ~/bin/firefox-stable/firefox ~/bin/firefox
+  ln -s ~/bin/Telegram/Telegram ~/bin/Telegram
+  ln -s ~/bin/tor-browser_en-US/Browser/start-tor-browser ~/bin/torbrowser
+fi
+
 ln -s ~/bin/firefox-dev/firefox ~/bin/firefox-dev
 ln -s ~/build/go-builds/bin/shfmt ~/bin/shfmt
-ln -s ~/bin/Telegram/Telegram ~/bin/Telegram
-ln -s ~/bin/tor-browser_en-US/Browser/start-tor-browser ~/bin/torbrowser
 ln -s ~/bin/acpilight/xbacklight ~/xbacklight
