d768f854fef0eb4de873211e36b20e519880f60e
Kos <spiritofsands@protonmail.com>
2018-02-12 09:38:46 +0200

    display handler should be run manually

--- display_handler.sh
+++ display_handler.sh
@@ -7,15 +7,17 @@ restart_stuff() {
   ~/.fehbg
 }
 
-case "$SRANDRD_ACTION" in
-  "HDMI-A-0 connected")
+status="$(cat /sys/class/drm/card0-HDMI-A-1/status)"
+echo "$status" >> /home/kos/status.log
+case "$status" in
+  "connected")
     xrandr --output eDP --off
     xrandr --output HDMI-A-0 --auto
-    restart_stuff
+    #restart_stuff
     ;;
-  "HDMI-A-0 disconnected")
+  "disconnected")
     xrandr --output HDMI-A-0 --off
     xrandr --output eDP --auto --fb 1366x768
-    restart_stuff
+    #restart_stuff
     ;;
 esac
