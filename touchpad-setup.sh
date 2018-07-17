c67216455146f938441c33e0502231c6c01de514
Kos <spiritofsands@protonmail.com>
2018-07-17 23:17:04 +0300

    Update battery and touch

--- touchpad-setup.sh
+++ touchpad-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ "$HOSTNAME" == 'kos-Inspiron-5565' ]]; then
-  xinput set-prop 'DELL0769:00 06CB:7E92 Touchpad' 'libinput Tapping Enabled' 1
+if [[ "$HOSTNAME" == 'kos-dell-laptop' ]]; then
+  xset m 20/10 45 r rate 300 20 b off
+  xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Tapping Enabled' 1
 fi
 
 if [[ "$HOSTNAME" == 'host-kos-pc' ]]; then
