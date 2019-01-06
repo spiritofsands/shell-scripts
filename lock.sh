1249ad411a84730156bd23cce01cdba06056c70c
Kostia Fursenko <spiritofsands@protonmail.com>
2019-01-07 00:40:22 +0200

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-readonly TMPBG=/tmp/screen.png
+
+readonly BG="$(grep -oP "(?<=').*(?=')" ~/.fehbg)"
 readonly LOCK=$HOME/bin/.lock.png
 readonly RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
-readonly UNUSED_WORKSPACE=7
+readonly TYPE='png'
 
-i3 workspace number "$UNUSED_WORKSPACE"
+TMPBG="/tmp/$(basename "$BG")"
+TMPBG="${TMPBG%\.*}.$TYPE"
 
-ffmpeg -f x11grab -video_size "$RES" -y -i "$DISPLAY" -i "$LOCK" -filter_complex "boxblur=5:10,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+if [[ ! -f "$TMPBG" ]]; then
+    ffmpeg -y -i "$BG" -i "$LOCK" -filter_complex \
+        "scale=$RES,boxblur=5:10,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
+        -vframes 1 "$TMPBG" -loglevel quiet
+fi
 
 pgrep compton > /dev/null
 readonly COMPTON_WAS_LAUNCHED=$?
@@ -21,6 +27,5 @@ if [[ "$COMPTON_WAS_LAUNCHED" == 0 ]]; then
     disown
 fi
 
-i3 workspace back_and_forth
-
-rm "$TMPBG"
+# delete $TYPE images older tnan 30 days
+find /tmp/ -name "*.$TYPE" -mtime +30 -type f -delete 2>/dev/null
