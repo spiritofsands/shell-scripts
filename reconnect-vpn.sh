3ee05f52278196adac24bb86ca594e35c5e2cca2
Kostia Fursenko <spiritofsands@protonmail.com>
2019-01-10 16:53:43 +0200

    Update vpn autoconnect

--- reconnect-vpn.sh
+++ reconnect-vpn.sh
@@ -4,7 +4,10 @@ while true; do
     if ! nmcli connection show --active | grep -q vpn; then
         notify-send 'VPN' 'Reconnecting...'
 
-        nmcli connection up "$(nmcli connection show | grep vpn | sed 's/ .*//')"
+        if ! nmcli connection up "$(nmcli connection show | grep vpn | sed 's/ .*//')"; then
+            echo 'nmcli failed'
+            exit 1
+        fi
     fi
 
     sleep 30
