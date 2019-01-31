68072dbb5b7024285662697e0facbcf0def86fff
Kostia Fursenko <spiritofsands@protonmail.com>
2019-01-31 16:56:45 +0200

    Slock

--- lock.sh
+++ lock.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-readonly BG="$(sed -e "s/feh.* '//" -e "s/' *$//" < <(tail -1 ~/.fehbg))"
-readonly LOCK=$HOME/bin/.lock.png
-readonly RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
-readonly TYPE='png'
-
-TMPBG="/tmp/$(basename "$BG")"
-TMPBG="${TMPBG%\.*}.$TYPE"
-
-if [[ ! -f "$TMPBG" ]]; then
-    ffmpeg -y -i "$BG" -i "$LOCK" -filter_complex \
-        "scale=$RES,boxblur=5:10,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" \
-        -vframes 1 "$TMPBG" -loglevel quiet
-fi
-
-i3lock --image="$TMPBG"
-
-# delete $TYPE images older tnan 30 days
-find /tmp/ -name "*.$TYPE" -mtime +30 -type f -delete 2>/dev/null
-
-exit 0
+slock
