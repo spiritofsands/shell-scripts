9f49e5c68e35d8f4666271237c7940ede12d0fcb
Kostia Fursenko <spiritofsands@protonmail.com>
2019-11-19 11:45:17 +0200

    Update battery status script to Debian 10

--- battery-status.sh
+++ battery-status.sh
@@ -79,11 +79,14 @@ battery_charge() {
                     battery_state=$(cat $battery_path/energy_now)
                     battery_full=$battery_path/energy_full
                     battery_current=$battery_path/energy_now
+                    now=$(cat $battery_current)
+                    full=$(cat $battery_full)
+                    BATT_PCT=$((100 * now / full))
                     ;;
                 *)
                     battery_state=$(cat $battery_path/status)
-                    battery_full=$battery_path/energy_full
-                    battery_current=$battery_path/energy_now
+                    battery_current=$battery_path/capacity
+                    BATT_PCT=$(cat $battery_current)
                     ;;
             esac
             if [ "$battery_state" == 'Discharging' ]; then
@@ -91,9 +94,6 @@ battery_charge() {
             else
                 BATT_CONNECTED=1
             fi
-                now=$(cat $battery_current)
-                full=$(cat $battery_full)
-                BATT_PCT=$((100 * now / full))
             ;;
     esac
 }
